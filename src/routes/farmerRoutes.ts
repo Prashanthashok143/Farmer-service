ccfbf67851aa790305edcfcd5e79c205d491c504
Prashanth <Prashanthashok143@gmail.com>
2025-11-08 20:08:59 +0530

    cloud signreq

--- src/routes/farmerRoutes.ts
+++ src/routes/farmerRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { FarmerController } from "../controllers/farmerController";
+import { generateCloudinaryPresignedUrl } from "../utils/cloudinaryUpload";
 const router = Router();
 
 router.post("/register", FarmerController.register);
+router.get("/cloudUploadSignature",generateCloudinaryPresignedUrl);
 
 export { router as farmerRoutes };
--- src/utils/cloudinaryUpload.ts
+++ src/utils/cloudinaryUpload.ts
@@ -1,14 +1,37 @@
 import cloudinary from "../config/cloudinary";
-// import streamifier from "streamifier";
-export const uploadToCloudinary = (fileBuffer: Buffer, folder: string) => {
-  return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream(
-      { folder },
-      (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
+import { Request, Response } from "express";
+
+
+export const generateCloudinaryPresignedUrl = async (req:Request , res: Response) => {
+  try {
+    const folder = req.query.folder as string;
+
+    //  Creating a timeStamp
+    const timestamp = Math.round(new Date().getTime() / 1000);
+
+    // Generating signature 
+    const paramsToSign = {
+      timestamp,
+      folder,
+      // resource_type: "auto",
+    }
+    const signature = cloudinary.utils.api_sign_request(paramsToSign , process.env.CLOUDINARY_API_SECRET as string);
+    res.status(200).json({
+      success: true,
+      data:{
+        signature,
+        timestamp,
+        api_key:process.env.CLOUDINARY_API_KEY,
+        cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+        folder
       }
-    );
-    // streamifier.createReadStream(fileBuffer).pipe(uploadStream);
-  });
-};
+    })
+  }
+  catch (error) {
+    console.log("Error generating Cloudinary presigned URL :" , error);
+    res.status(500).json({
+      success: false,
+      message: "Error generating Cloudinary presigned URL",
+    })
+  }
+}
